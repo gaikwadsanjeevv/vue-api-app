678d67fdfdee2838397de116ef6ee79aacd96cc3
Sanjeev V Gaikwad <161517224+gaikwadsanjeevv@users.noreply.github.com>
2025-03-13 21:57:09 -0500

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+
+  devServer: {
+    allowedHosts: "all",
+    client: {
+      webSocketURL: "wss://0.0.0.0:443/ws", // Fix WebSocket issue in GitHub Codespaces
+      overlay: false, // Disable WebSocket error overlay
+    },
+  },
+});
